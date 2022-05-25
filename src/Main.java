0d992e631efb9d75a17dd4cd68425fd7b2000c9f
Sebastian Santos <72042983+Sebass83@users.noreply.github.com>
2022-05-25 20:22:58 -0300

    Update Main.java
    Primera parte:
    
    Crear una función con tres parámetros que sean números que se suman entre sí.
    
    Llamar a la función en el main y darle valores.
    
    Segunda parte:
    
    Crear una clase coche.
    
    Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
    
    Una función que incremente el número de puertas que tiene el coche.
    
    Crear un objeto miCoche en el main y añadirle una puerta.
    
    Mostrar el número de puertas que tiene el objeto.

--- src/Main.java
+++ src/Main.java
@@ -23,15 +23,6 @@ public class Main {
 
 
     }
-
-    //Segunda parte:
-    //Crear una clase coche.
-    //Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
-    //Una función que incremente el número de puertas que tiene el coche.
-    //Crear un objeto miCoche en el main y añadirle una puerta
-
-
-
 }
 //Segunda parte:
 //Crear una clase coche.
